b6957fe49d009bc5ff660352c58eaf8101a6f406
Gilles <g.lautrou@outlook.com>
2017-05-07 18:28:47 +0200

    Create page template using bootstrap

--- src/App.js
+++ src/App.js
@@ -5,15 +5,40 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div>
+        <nav className="navbar navbar-default navbar-fixed-top">
+        <div className="container">
+          <div className="navbar-header">
+            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
+            <a className="navbar-brand" href="#">Messaging</a>
+          </div>
+          <div id="navbar" className="collapse navbar-collapse">
+            <ul className="nav navbar-nav">
+              <li className="active"><a href="#">Home</a></li>
+              <li><a href="#about">About</a></li>
+            </ul>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      </nav>
+
+      <div id="page" className="container">
+        <div className="page-header">
+          <h1>Main page</h1>
+        </div>
+        <p>Content of the main page.</p>
       </div>
+
+      <footer className="footer">
+        <div className="container">
+          <p className="text-muted">Copyright</p>
+        </div>
+      </footer>
+    </div>
     );
   }
 }
