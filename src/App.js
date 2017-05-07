215713d0c2f03937d0b4afce71752802b9150b20
Gilles <g.lautrou@outlook.com>
2017-05-07 22:10:09 +0200

    Add basic page navigation for home and about pages

--- src/App.js
+++ src/App.js
@@ -1,42 +1,54 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  IndexRoute,
+  Link,
+  IndexLink,
+  NavLink
+} from 'react-router-dom';
 import Home from './Home';
+import About from './About';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <nav className="navbar navbar-default navbar-fixed-top">
-        <div className="container">
-          <div className="navbar-header">
-            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-              <span className="sr-only">Toggle navigation</span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-            </button>
-            <a className="navbar-brand" href="#">Messaging</a>
+      <Router>
+        <div>
+          <nav className="navbar navbar-default navbar-fixed-top">
+          <div className="container">
+            <div className="navbar-header">
+              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+              <Link to="/" className="navbar-brand">Messaging</Link>
+            </div>
+            <div id="navbar" className="collapse navbar-collapse">
+              <ul className="nav navbar-nav">
+                <li><NavLink exact to="/" activeClassName="active">Home</NavLink></li>
+                <li><NavLink to="/about" activeClassName="active">About</NavLink></li>
+              </ul>
+            </div>
           </div>
-          <div id="navbar" className="collapse navbar-collapse">
-            <ul className="nav navbar-nav">
-              <li className="active"><a href="#">Home</a></li>
-              <li><a href="#about">About</a></li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+        </nav>
 
-      <div id="page" className="container">
-        <Home />
+        <div id="page" className="container">
+          <Route exact path="/" component={Home}/>
+          <Route path="/about" component={About}/>
+        </div>
+
+        <footer className="footer">
+          <div className="container">
+            <p className="text-muted">Copyright</p>
+          </div>
+        </footer>
       </div>
-
-      <footer className="footer">
-        <div className="container">
-          <p className="text-muted">Copyright</p>
-        </div>
-      </footer>
-    </div>
+    </Router>
     );
   }
 }
