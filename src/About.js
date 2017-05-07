955e325cc9798c293724886fe06d8a3a6a03a473
Gilles <g.lautrou@outlook.com>
2017-05-07 18:38:20 +0200

    Create empty about page

---
+++ src/About.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <div className="page-header">
+          <h1>About page</h1>
+        </div>
+        <p>Content of the about page.</p>
+      </div>
+    );
+  }
+}
+
+export default About;
