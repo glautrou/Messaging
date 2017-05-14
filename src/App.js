a31120b0b45602bd5f5cb82c53e9bb816030afef
Gilles <g.lautrou@outlook.com>
2017-05-14 13:29:01 +0200

    Fix indentation

--- src/App.js
+++ src/App.js
@@ -18,37 +18,37 @@ class App extends Component {
       <Router>
         <div>
           <nav className="navbar navbar-default navbar-fixed-top">
-          <div className="container">
-            <div className="navbar-header">
-              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span className="sr-only">Toggle navigation</span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-              </button>
-              <Link to="/" className="navbar-brand">Messaging</Link>
+            <div className="container">
+              <div className="navbar-header">
+                <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                  <span className="sr-only">Toggle navigation</span>
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+                </button>
+                <Link to="/" className="navbar-brand">Messaging</Link>
+              </div>
+              <div id="navbar" className="collapse navbar-collapse">
+                <ul className="nav navbar-nav">
+                  <li><NavLink exact to="/" activeClassName="active">Home</NavLink></li>
+                  <li><NavLink to="/about" activeClassName="active">About</NavLink></li>
+                </ul>
+              </div>
             </div>
-            <div id="navbar" className="collapse navbar-collapse">
-              <ul className="nav navbar-nav">
-                <li><NavLink exact to="/" activeClassName="active">Home</NavLink></li>
-                <li><NavLink to="/about" activeClassName="active">About</NavLink></li>
-              </ul>
-            </div>
-          </div>
-        </nav>
+          </nav>
 
-        <div id="page" className="container">
-          <Route exact path="/" component={Home}/>
-          <Route path="/about" component={About}/>
+          <div id="page" className="container">
+            <Route exact path="/" component={Home}/>
+            <Route path="/about" component={About}/>
+          </div>
+
+          <footer className="footer">
+            <div className="container">
+              <p className="text-muted">Copyright</p>
+            </div>
+          </footer>
         </div>
-
-        <footer className="footer">
-          <div className="container">
-            <p className="text-muted">Copyright</p>
-          </div>
-        </footer>
-      </div>
-    </Router>
+      </Router>
     );
   }
 }
